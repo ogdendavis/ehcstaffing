8da3685c82c87fefd3fed6f85fd354a7ab40bef9
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-05-22 10:27:36 -0400

    Move layout to separate component

--- wp-content/themes/ehcstaffing/react-src/src/App.js
+++ wp-content/themes/ehcstaffing/react-src/src/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-// Page templates
-import Home from './templates/Home';
-import About from './templates/About';
-import BlogArchive from './templates/BlogArchive';
-import BlogSingle from './templates/BlogSingle';
-import Contact from './templates/Contact';
-import JobArchive from './templates/JobArchive';
-import JobSingle from './templates/JobSingle';
-import FourOhFour from './templates/404';
-
-// Components
-import Header from './components/Header';
+import Layout from './components/Layout';
 
 const App = () => {
   const testPath = '/' + process.env.REACT_APP_HOME.split('/').pop();
@@ -21,34 +10,7 @@ const App = () => {
 
   return (
     <BrowserRouter basename={baseName}>
-      <Header />
-      <Switch>
-        {/* Remember that routes should go from most to least specific */}
-        <Route path="/about" key="about_route">
-          <About />
-        </Route>
-        <Route path="/blog/:id" key="blog_single_route">
-          <BlogSingle />
-        </Route>
-        <Route path="/blog" key="blog_archive_route">
-          <BlogArchive />
-        </Route>
-        <Route path="/contact" key="contact_route">
-          <Contact />
-        </Route>
-        <Route path="/jobs/:id" key="job_single_route">
-          <JobSingle />
-        </Route>
-        <Route path="/jobs" key="job_archive_route">
-          <JobArchive />
-        </Route>
-        <Route exact path="/" key="home">
-          <Home />
-        </Route>
-        <Route key="404_route">
-          <FourOhFour />
-        </Route>
-      </Switch>
+      <Layout />
     </BrowserRouter>
   );
 };
---
+++ wp-content/themes/ehcstaffing/react-src/src/components/Layout.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Page templates
+import Home from '../templates/Home';
+import About from '../templates/About';
+import BlogArchive from '../templates/BlogArchive';
+import BlogSingle from '../templates/BlogSingle';
+import Contact from '../templates/Contact';
+import JobArchive from '../templates/JobArchive';
+import JobSingle from '../templates/JobSingle';
+import FourOhFour from '../templates/404';
+
+// Components
+import Header from '../components/Header';
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Switch>
+        {/* Remember that routes should go from most to least specific */}
+        <Route path="/about" key="about_route">
+          <About />
+        </Route>
+        <Route path="/blog/:id" key="blog_single_route">
+          <BlogSingle />
+        </Route>
+        <Route path="/blog" key="blog_archive_route">
+          <BlogArchive />
+        </Route>
+        <Route path="/contact" key="contact_route">
+          <Contact />
+        </Route>
+        <Route path="/jobs/:id" key="job_single_route">
+          <JobSingle />
+        </Route>
+        <Route path="/jobs" key="job_archive_route">
+          <JobArchive />
+        </Route>
+        <Route exact path="/" key="home">
+          <Home />
+        </Route>
+        <Route key="404_route">
+          <FourOhFour />
+        </Route>
+      </Switch>
+    </>
+  );
+};
+
+export default Layout;
