7e30cac3c45214199f8a2491a4b6e54f27d95897
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-05-29 15:27:32 -0400

    Pipe WP content into About page

--- wp-content/themes/nurseteam/react-src/src/templates/About.js
+++ wp-content/themes/nurseteam/react-src/src/templates/About.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const AboutPageMain = styled.main`
+  max-width: ${props => props.theme.contentWidth};
+  padding: 0 2rem 2rem;
+  margin: 0 auto;
+  overflow: hidden;
+
+  img {
+    max-width: 100%;
+  }
+`;
 
 const About = props => {
-  return <div>This is the template for displaying the about us page</div>;
+  const [page, setPage] = useState(false);
+
+  useEffect(() => {
+    async function getAboutPage() {
+      await fetch(
+        `${process.env.REACT_APP_HOME}/wp-json/wp/v2/pages?_slug=about`
+      )
+        .then(res => res.json())
+        .then(j => setPage(j[0]));
+    }
+    getAboutPage();
+  }, []);
+
+  return (
+    <AboutPageMain>
+      {page && (
+        <>
+          <h1>{page.title.rendered}</h1>
+          <div dangerouslySetInnerHTML={{ __html: page.content.rendered }} />
+        </>
+      )}
+    </AboutPageMain>
+  );
 };
 
 export default About;
