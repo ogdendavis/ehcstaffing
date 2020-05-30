d4524aa63f0d6a4e1a359cc5e8ab35475e6d4d3a
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-05-30 14:19:04 -0400

    Style blog listing

--- wp-content/themes/nurseteam/react-src/src/components/Button.js
+++ wp-content/themes/nurseteam/react-src/src/components/Button.js
@@ -2,9 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ButtonContainer = styled.button`
-  cursor: pointer;
+  cursor: ${props => (props.inactive ? 'default' : 'pointer')};
   background: ${props =>
-    props.highlighted ? props.theme.secondaryColor : props.theme.primaryColor};
+    props.highlighted
+      ? props.theme.secondaryColor
+      : props.inactive
+      ? 'gray'
+      : props.theme.primaryColor};
   padding: 0.75rem 1.5rem;
   color: #fff;
   border-radius: 100px;
@@ -13,16 +17,17 @@ const ButtonContainer = styled.button`
   transition: ${props => props.theme.transition};
 
   &:hover {
-    transform: scale(1.1);
+    transform: ${props => (props.inactive ? 'none' : 'scale(1.1)')};
   }
 `;
 
-const Button = ({ text, highlighted, handleClick, value }) => {
+const Button = ({ text, highlighted, handleClick, value, inactive }) => {
   return (
     <ButtonContainer
       highlighted={highlighted}
       onClick={handleClick}
       value={value}
+      inactive={inactive}
     >
       {text}
     </ButtonContainer>
--- wp-content/themes/nurseteam/react-src/src/templates/BlogArchive.js
+++ wp-content/themes/nurseteam/react-src/src/templates/BlogArchive.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+import Button from '../components/Button';
 
 const BlogPageMain = styled.main`
   max-width: ${props => props.theme.contentWidth};
@@ -7,28 +10,44 @@ const BlogPageMain = styled.main`
   margin: 0 auto;
 `;
 
+const PostTease = styled(Link)`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  text-decoration: none;
+  color: ${props => props.theme.textColor};
+  transition: background-color 0.5s ease;
+
+  &:hover {
+    background-color: ${props => props.theme.primaryColor + `55`};
+  }
+
+  h2 {
+    margin-top: 0;
+  }
+
+  p {
+    font-size: 0.8rem;
+    font-style: italic;
+  }
+`;
+
 const BlogNav = styled.div`
   padding: 1rem;
-  background: black;
-  color: white;
+  display: flex;
+  justify-content: space-between;
+
+  .inactive {
+    background: gray;
+    color: black;
+    cursor: default;
+  }
 `;
 
 const BlogArchive = props => {
   const [posts, setPosts] = useState(false);
   const [numPages, setNumPages] = useState(0);
   const [page, setPage] = useState(1);
-
-  async function getPosts() {
-    await fetch(
-      `${process.env.REACT_APP_HOME}/wp-json/wp/v2/posts?page=${page}`
-    )
-      .then(res => {
-        // Use WP-provided header to indicate the number of pages of blog posts we have
-        setNumPages(Number(res.headers.get('x-wp-totalpages')));
-        return res.json();
-      })
-      .then(j => setPosts(j));
-  }
 
   const prevPosts = () => {
     if (page > 1) {
@@ -44,6 +63,17 @@ const BlogArchive = props => {
 
   // Get the posts on load, and when the page changes
   useEffect(() => {
+    async function getPosts() {
+      await fetch(
+        `${process.env.REACT_APP_HOME}/wp-json/wp/v2/posts?page=${page}`
+      )
+        .then(res => {
+          // Use WP-provided header to indicate the number of pages of blog posts we have
+          setNumPages(Number(res.headers.get('x-wp-totalpages')));
+          return res.json();
+        })
+        .then(j => setPosts(j));
+    }
     getPosts();
   }, [page]);
 
@@ -51,11 +81,22 @@ const BlogArchive = props => {
     <BlogPageMain>
       {posts &&
         posts.map(post => (
-          <div key={`post${post.id}`}>{post.title.rendered}</div>
+          <PostTease to={`/articles/${post.id}`} key={`post${post.id}`}>
+            <h2>{post.title.rendered}</h2>
+            <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
+          </PostTease>
         ))}
       <BlogNav>
-        <button onClick={prevPosts}>Prev</button>
-        <button onClick={nextPosts}>Next</button>
+        <Button
+          inactive={page === 1}
+          handleClick={prevPosts}
+          text="&lt; Prev"
+        />
+        <Button
+          inactive={page === numPages}
+          handleClick={nextPosts}
+          text="Next &gt;"
+        />
       </BlogNav>
     </BlogPageMain>
   );
