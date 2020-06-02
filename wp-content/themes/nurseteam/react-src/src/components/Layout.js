339e3fc8ed9e8e2c205078b977410f145b4c2aa5
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-06-02 15:51:20 -0400

    Refactor router, remove JobSingle

--- wp-content/themes/nurseteam/react-src/src/components/Layout.js
+++ wp-content/themes/nurseteam/react-src/src/components/Layout.js
@@ -9,7 +9,6 @@ import BlogArchive from '../templates/BlogArchive';
 import BlogSingle from '../templates/BlogSingle';
 import Contact from '../templates/Contact';
 import JobArchive from '../templates/JobArchive';
-import JobSingle from '../templates/JobSingle';
 import FourOhFour from '../templates/404';
 
 // Components
@@ -31,30 +30,21 @@ const Layout = () => {
         <Header />
         <Switch>
           {/* Remember that routes should go from most to least specific */}
-          <Route path="/about" key="about_route">
-            <About />
-          </Route>
-          <Route path="/articles/:id" key="blog_single_route">
-            <BlogSingle />
-          </Route>
-          <Route path="/articles" key="blog_archive_route">
-            <BlogArchive />
-          </Route>
-          <Route path="/contact" key="contact_route">
-            <Contact />
-          </Route>
-          <Route path="/jobs/:id" key="job_single_route">
-            <JobSingle />
-          </Route>
-          <Route path="/jobs" key="job_archive_route">
-            <JobArchive />
-          </Route>
-          <Route exact path="/" key="home">
-            <Home />
-          </Route>
-          <Route key="404_route">
-            <FourOhFour />
-          </Route>
+          <Route path="/about" key="about_route" component={About} />
+          <Route
+            path="/articles/:id"
+            key="blog_single_route"
+            component={BlogSingle}
+          />
+          <Route
+            path="/articles"
+            key="blog_archive_route"
+            component={BlogArchive}
+          />
+          <Route path="/contact" key="contact_route" component={Contact} />
+          <Route path="/jobs" key="job_archive_route" component={JobArchive} />
+          <Route exact path="/" key="home" component={Home} />
+          <Route key="404_route" component={FourOhFour} />
         </Switch>
       </PageContainer>
       <Footer />
--- wp-content/themes/nurseteam/react-src/src/templates/JobSingle.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const JobSingle = props => {
-  return <div>This is the template for displaying a single job</div>;
-};
-
-export default JobSingle;
