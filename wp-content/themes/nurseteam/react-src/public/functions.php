741da13519de8a91109f50307e7c3e0299a82236
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-06-15 11:32:30 -0400

    Remove capability limitation for editors

--- wp-content/themes/nurseteam/react-src/public/functions.php
+++ wp-content/themes/nurseteam/react-src/public/functions.php
@@ -4,8 +4,6 @@
 require_once __DIR__ . '/include/jobs.php';
 // Customize back-end dashboard
 require_once __DIR__ . '/include/dashboard.php';
-// Remove add/delete page capability for editors
-require_once __DIR__ . '/include/capabilities.php';
 // API init
 require_once __DIR__ . '/include/api_jobs.php';
 // Handle form submissions
