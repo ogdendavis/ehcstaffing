e84177e01734de561b3cc6c5d8cef75607312bdb
Lucas Ogden-Davis <lucas@ogdendavis.com>
2020-06-24 14:15:52 -0400

    Add protection against missing values in jobs api

--- wp-content/themes/nurseteam/react-src/public/include/api_jobs.php
+++ wp-content/themes/nurseteam/react-src/public/include/api_jobs.php
@@ -27,19 +27,60 @@ if (!function_exists('ehc_get_jobs')) {
             },
             ARRAY_FILTER_USE_KEY
         );
+
         // Simplify by making keys plain English and bringing the info up out
         // of the array it's currently hiding in
         foreach ($filtered as $k => $v) {
             $newkey = str_replace('_job_', '', $k);
             $simplified[$newkey] = $v[0];
         }
+
+        // Because we're uploading spreadsheets, things can get weird. Insert default values into API response if values were missing from spreadsheet.
+        if (!array_key_exists('sourceid', $simplified)) {
+            // Generate random ID with numbers and characters, so the odds of accidentally matching an existing one are low.
+            $chars = '0123456789qwertyuiopasdfghjklzxcvbnm';
+            $rando_id = '';
+            for ($i = 0; $i < 10; $i++) {
+                $rando_id .= $chars[rand(0, 35)];
+            }
+            $simplified['sourceid'] = $rando_id;
+        }
+        if (!array_key_exists('city', $simplified)) {
+            $simplified['city'] = '';
+        }
+        if (!array_key_exists('state', $simplified)) {
+            $simplified['state'] = '';
+        }
+        if (!array_key_exists('startdate', $simplified)) {
+            $simplified['startdate'] = '';
+        }
+        if (!array_key_exists('duration', $simplified)) {
+            $simplified['duration'] = '';
+        }
+        if (!array_key_exists('specialty', $simplified)) {
+            $simplified['specialty'] = '';
+        }
+        if (!array_key_exists('unit', $simplified)) {
+            $simplified['unit'] = '';
+        }
+        if (!array_key_exists('shift', $simplified)) {
+            $simplified['shift'] = '';
+        }
+        if (!array_key_exists('pay', $simplified)) {
+            $simplified['pay'] = '';
+        }
+        if (!array_key_exists('description', $simplified)) {
+            $simplified['description'] = '';
+        }
+
         // Add title for consistent display in job listings and submission form
         $simplified['display_title'] =
-            $simplified['specialty'] .
+            $simplified['pay'] .
             ' in ' .
             $simplified['city'] .
             ', ' .
             $simplified['state'];
+        return $simplified;
         // Add local id to simplify fetching job data from associated applications
         $simplified['localid'] = $job_post->ID;
         return $simplified;
